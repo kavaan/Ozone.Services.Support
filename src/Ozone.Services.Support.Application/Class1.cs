d530b3dde06e63580da25ca7382dc0e5f4294565
Samin <Samin@DESKTOP-B8FQBKL>
2020-08-18 00:02:15 +0430

    first commit

---
+++ src/Ozone.Services.Support.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Ozone.Services.Support.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/Ozone.Services.Support.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Ozone.Services.Support.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
